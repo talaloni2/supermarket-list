9e964ff926e1d56f565b1fccc8a8aff6c2ef513a
Tal Aloni <tal.aloni@planckresolution.com>
2022-11-24 22:56:43 +0200

    Added capitalization

--- src/components/item-details/NameWithLogo.js
+++ src/components/item-details/NameWithLogo.js
@@ -2,9 +2,10 @@ import data from "../data.json"
 import "./NameWithLogo.css"
 
 export default function NameWithLogo(props) {
+    let capitalizedName = props.name.charAt(0).toUpperCase() + props.name.slice(1);;
     return (
         <div className="nameWithLogo">
-            <div className="name">{props.name}</div>
+            <div className="name">{capitalizedName}</div>
             <img src={props.photo} alt=":(" height={100} width={100}/>
         </div>
     )
